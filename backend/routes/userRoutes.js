f771953562ae2cf519bef5453c666e490d8ae11d
RajGuptaVips2025 <rajguptavips2025@gmail.com>
2024-10-17 21:16:03 +0530

    typing field integrated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const { getUserAndPosts, getFollowing, following, updateProfile } = require('../controllers/userController');
-// const upload = require('../middlewares/uploadMiddleware');
-// const router = express.Router();
-
-// router.get('/:username', getUserAndPosts);
-// router.post('/edit/:id', upload.single('media'), updateProfile);
-// router.get('/:id/following', getFollowing);
-// router.put('/:id/following', following);
-
-// module.exports = router;
-
 const express = require('express');
 const { getUserAndPosts, getFollowing, following, updateProfile, addToReelHistory } = require('../controllers/userController');
 const upload = require('../middlewares/uploadMiddleware');
