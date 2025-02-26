635f33aa92dc826ac687da67a2149e78f1c1ac71
Raj Gupta <168336609+RajGuptaVips2025@users.noreply.github.com>
2025-02-27 00:23:39 +0530

    Update api.js

--- frontend/src/components/Auth/api.js
+++ frontend/src/components/Auth/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create ({
-    baseURL: 'http://localhost:5000/api/auth'
+    baseURL: 'https://instagram-backend-qqjd.onrender.com/api/auth'
 })
 
 export const googleAuth = (code) => api.post(`/google?code=${code}`);
