7b155759d3f54a7e62154514123fb928522b1447
RajGuptaVips2025 <rajguptavips2025@gmail.com>
2025-02-28 23:51:02 +0530

    Database changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected',process.env.MONGO_URI);
+    console.log("MongoDB connected:", process.env.MONGO_URI);
   } catch (err) {
-    console.error(err.message);
+    console.error("MongoDB connection error:", err);
     process.exit(1);
   }
 };
+
 
 module.exports = connectDB;
